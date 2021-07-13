38cd80519302d027177051aee655c1a653e5b3ee
Winston <niera.w.a@gmail.com>
2021-07-13 19:52:35 -0400

    added homeroutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,20 @@
+<<<<<<< HEAD
+const path = require('path');
 
+module.exports = (app) => {
+    app.get('/customer', (req,res) => {
+        res.sendfile(path.join(__dirname, '../public/customer.html'));
+    });
+
+    app.get('/employee', (req,res) => {
+        res.sendfile(path.join(__dirname, '../public/employee.html'));
+    });
+
+    app.get('*', (req,res) => {
+        res.sendfile(path.join(__dirname, '../public/index.html'));
+    });
+}
+=======
 const router = require('express').Router();
 const { Customer, Employee, Job } = require('../models');
 const withAuth = require('../utils/auth');

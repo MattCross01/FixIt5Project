a87b7e8c05af0f793a385b1005ac7ebef2f885c0
Cameron Rushing <cameronrushing54@gmail.com>
2021-07-15 20:37:21 -0400

    login js 2

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
+import Login from "./containers/Login";
 
 export default function Login() {
   const [email, setEmail] = useState("");

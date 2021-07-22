6ae3be4c6c6bce131eea3fe7162fa798c8a7896d
Cameron Rushing <cameronrushing54@gmail.com>
2021-07-22 13:24:49 -0400

    login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
-
+import { Auth } from "aws-amplify";
 
 export default function Login() {
   const [email, setEmail] = useState("");

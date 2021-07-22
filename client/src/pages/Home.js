f2de9609a34c9bd0fef641fd5ca24bbad5349da0
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-22 18:15:44 -0400

    Modified front page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -68,39 +68,28 @@ class Home extends Component {
                     <Row>
                     <Col size="md-12">
                         <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-                        consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-                        parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-                        sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-                        gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-                        turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-                        tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-                        quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-                        volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-                        mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-                        tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+                        Who is FIXIT5?
+FIXIT5 isn't the guy offering home repair services from the back of his pickup truck. FIXIT5 is your personal home improvement consultant and a trusted, knowledgeable resource that is part of a leading national service franchise. 
+Our experienced, professional home repair and improvement technicians are skilled craftsmen with an average of 10 years of experience in the trades. We’re so confident in the work we perform that each job we do, whether a repair, installation, assembly or organization task, is backed by our Done Right Guarantee.
+You don’t have time to spend on a service that’s less than reliable and dependable, and you shouldn’t let just anyone in your home. When you need professional handyman services you can trust and depend on to get it done right, you can count on FIXIT5.
                         </p>
                         <p>
-                        Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-                        malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-                        ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-                        nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-                        Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-                        porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
+                        Repeat customers from across the country love the convenience of working with us, and we love nothing better than improving your home; making it safer, more functional and simply a more comfortable place to live. Contact your local FIXIT5 today.
                         </p>
                         <p>
-                        Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-                        tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
-                        sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-                        Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-                        congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-                        vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-                        nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-                        Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-                        libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-                        eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-                        interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-                        massa.
+                        FIXIT5 is your one-call solution for a wide range of home maintenance and repair needs. Our uniformed technicians are fully insured professionals. We arrive on time in uniform and a marked van with the tools to complete the job right.
+
+FIXIT5 technicians are highly skilled craftsmen with an average of 10 years’ experience in the trades. We’re home repair and improvement experts, and we are known for the quality of our workmanship and professional reliability so you know the job will be done correctly and efficiently.
+
+FIXIT, was founded in 2010 and has served nearly 1 million customers across North America. Here are a few reasons why our customers recommend FIXIT5 to their friends.
+
+One simple call provides easy scheduling
+On time arrival
+Job is done right the first time
+Wide variety of repair, maintenance and improvements services
+Technicians have an average of 10 years’ experience in the trades and are fully insured for your safety
+We promise to clean up when we’re done
+Our quality workmanship is guaranteed
                         </p>
                     </Col>
                     </Row>
